2eaa7aea1500e4218b2181b5235a388a00f286a6
jmvalick <jmvalick@gmail.com>
2024-11-18 21:23:11 -0500

    Exceptions, Inputs

--- src/main/java/org/example/ExceptionDemo.java
+++ src/main/java/org/example/ExceptionDemo.java
@@ -1,23 +1,69 @@
 package org.example;
 
-import jdk.incubator.vector.VectorOperators;
+class NewException extends RuntimeException
+{
+    public NewException(String string){
+        super(string);
+    }
+}
 
-import java.awt.*;
+class AA{
+    public void show() throws ClassNotFoundException{
+        Class.forName("Calc");
+    }
+}
 
 public class ExceptionDemo {
+    static{
+        System.out.println("Class Loaded");
+    }
+
     public static void main(String a[])
     {
-        int i = 0;
+        int i = 20;
         int j = 0;
 
         try{
             j = 18/i;
+            if(j==0)
+                throw new NewException("I don't want to print 0");
         }
-        catch (Exception e){
-            System.out.println("Divide by zero");
+        catch(NewException e){
+            j = 18/1;
+            //System.out.println(e);
         }
+        //System.out.println(j + "\n");
 
-        System.out.println(j);
 
+        int nums[] = new int[5];
+
+        try
+        {
+            System.out.println(nums[5]);
+            j = 20/0;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            //System.out.println("1: " + e);
+        }
+        catch(ArithmeticException e) {
+            //System.out.println("2: " + e);
+        }
+        catch(Exception e){
+            //System.out.println("3: " + e);
+        }
+        //System.out.println(j);
+
+
+        AA obj = new AA();
+        try{
+            obj.show();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("finally");
+            // close something here
+        }
     }
 }
---
+++ src/main/java/org/example/InputDemo.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class InputDemo {
+    public static void main(String a[]) throws IOException {
+/*        System.out.println("Enter number");
+        int num1 = System.in.read();
+        System.out.println(num1);  // prints ascii
+
+ */
+
+        System.out.println("Enter number");
+        InputStreamReader in = new InputStreamReader(System.in);
+        try(BufferedReader bf = new BufferedReader(in);){
+            int num2 = Integer.parseInt(bf.readLine());
+            System.out.println(num2);
+            // bf.close(); // redundant
+        }
+
+
+        System.out.println("Enter number");
+        Scanner sc = new Scanner(System.in);
+        int num3 = sc.nextInt();
+        System.out.println(num3);
+    }
+}
