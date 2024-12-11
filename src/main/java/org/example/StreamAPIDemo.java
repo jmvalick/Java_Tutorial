56047a1ac13885b455b05f37eaf9a729d0c6248a
jmvalick <jmvalick@gmail.com>
2024-12-10 22:07:39 -0500

    Streams

---
+++ src/main/java/org/example/StreamAPIDemo.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class StreamAPIDemo {
+    public static void main(String a[]){
+        List<Integer> nums = Arrays.asList(4,5,7,3,2,6);
+
+//        Stream<Integer> s1 = nums.stream();
+//        Stream<Integer> s2 = s1.filter(n -> n%2==0);
+//        Stream<Integer> s3 = s2.map(n -> n*2);
+//        int result = s3.reduce(0, (c,e) -> c+e);
+
+        Predicate<Integer> p = n -> (n%2 == 0);
+
+        int result =    nums.stream()
+                            .filter(p)
+                            .map(n -> n*2)
+                            .sorted()
+                            .reduce(0, (c,e) -> c+e);
+
+        //s3.forEach(n -> System.out.println(n));
+        System.out.println(result);
+    }
+
+}
